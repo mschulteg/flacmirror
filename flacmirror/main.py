e4a072a11f406e3604d65aef26bfdb9c175ddbd9
Moritz Schulte <mschulte.g@gmail.com>
2021-11-13 13:46:59 +0100

    Add descriptions to argparse.

--- flacmirror/main.py
+++ flacmirror/main.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+import argparse
 from pathlib import Path
 import signal
 
@@ -9,20 +9,109 @@ from .options import Options
 
 def main():
     codecs = ["vorbis", "opus"]
-    argparser = ArgumentParser()
-    argparser.add_argument("src_dir")
-    argparser.add_argument("dst_dir")
-    argparser.add_argument("--codec", type=str, choices=codecs)
-    argparser.add_argument(
-        "--albumart", type=str, choices=["optimize", "resize", "keep", "discard"]
+    argparser = argparse.ArgumentParser(
+        description=(
+            "Program to recursively synchronize a directory containing flac files to"
+            " another directory while encoding the flac files instead of copying them."
+        ),
+        formatter_class=lambda prog: argparse.HelpFormatter(
+            prog, max_help_position=48, width=120
+        ),
     )
-    argparser.add_argument("--albumart-max-width", type=int, default=750)
-    argparser.add_argument("--overwrite", type=str, choices=["all", "none", "old"])
-    argparser.add_argument("--delete", action="store_true")
-    argparser.add_argument("--copy-files", type=str, default="")
-    argparser.add_argument("--num-threads", type=int, default=None)
-    argparser.add_argument("--opus-quality", type=float, default=None)
-    argparser.add_argument("--vorbis-quality", type=int, default=None)
+    argparser.add_argument(
+        "src_dir",
+        help=(
+            "The source directory. This directory will be recursively scanned for flac"
+            " files to be encoded."
+        ),
+    )
+    argparser.add_argument(
+        "dst_dir",
+        help=(
+            "The destination directory. Encoded files will be saved here using the same"
+            " directory structure as in src_dir."
+        ),
+    )
+    argparser.add_argument(
+        "--codec", type=str, choices=codecs, help="Specify which target codec to use."
+    )
+    argparser.add_argument(
+        "--albumart",
+        type=str,
+        default="optimize",
+        choices=["optimize", "resize", "keep", "discard"],
+        help=(
+            "Specify what to do with album covers. Defaults to 'optimize'. 'optimize'"
+            " will try to optimize the picture for better size, while 'resize' will"
+            " additionally downsample pictures with a pixel width greater than"
+            " --albumart-max-width (default 750) while scaling the height"
+            " proportionally. 'keep' will just copy the album art over to the new file,"
+            " while 'discard' will not add the album art to the encoded file."
+        ),
+    )
+    argparser.add_argument(
+        "--albumart-max-width",
+        type=int,
+        default=750,
+        help=(
+            "Specify the width in pixels to which album art is downscaled to (if"
+            " greater). Defaults to 750. Only used when --albumart is set to resize."
+        ),
+    )
+    argparser.add_argument(
+        "--overwrite",
+        type=str,
+        default="old",
+        choices=["all", "none", "old"],
+        help=(
+            "Specify if or when existing files should be overwritten. 'all' means that"
+            " files are always overwritten, 'none' means that files are never"
+            " overwritten and 'old' means that files are only overwritten if the source"
+            " file has changed since (the source file's modification date is newer)."
+        ),
+    )
+    argparser.add_argument(
+        "--delete",
+        action="store_true",
+        help="Delete files that exist at the destination but not the source.",
+    )
+    argparser.add_argument(
+        "--copy-files",
+        type=str,
+        default="",
+        help=(
+            "Copy additional files that are not being encoded. COPY_FILES is a list of"
+            " filenames separated by ','. For example --copy-files"
+            ' "cover.jpg,albumart.jpg"'
+        ),
+    )
+    argparser.add_argument(
+        "--num-threads",
+        type=int,
+        default=None,
+        help=(
+            "Number of threads to use. Defaults to the number of threads in the system."
+        ),
+    )
+    argparser.add_argument(
+        "--opus-quality",
+        type=float,
+        default=None,
+        help=(
+            "If opus encoding was selected, the bitrate in kbit/s can be specified as a"
+            " float. The value is direclty passed to the --bitrate argument of opusenc."
+        ),
+    )
+    argparser.add_argument(
+        "--vorbis-quality",
+        type=int,
+        default=None,
+        help=(
+            "If vorbis encoding was selected, the quality can be specified as an"
+            " integer between -1 and 10. The value is directly passed to the --quality"
+            " argument of oggenc."
+        ),
+    )
     arg_results = argparser.parse_args()
     options = Options(
         src_dir=Path(arg_results.src_dir),
